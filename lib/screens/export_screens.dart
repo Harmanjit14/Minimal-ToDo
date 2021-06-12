510668e95a21464a520cfea147819d413d9faaec
Harmanjit Singh <harmanjit140500@gmail.com>
2021-06-12 10:54:48 +0530

    base1

---
+++ lib/screens/export_screens.dart
@@ -0,0 +1,1 @@
+export 'screenA.dart';
---
+++ lib/screens/screenA.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScreenA extends StatelessWidget {
+  const ScreenA({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
